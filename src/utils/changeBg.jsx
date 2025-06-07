6c893379519ffedd1ea99c43010c5785f82900fb
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-06-07 14:07:13 +0530

    feat: addition of more gradiants

--- src/utils/changeBg.jsx
+++ src/utils/changeBg.jsx
@@ -1,16 +1,28 @@
 const changeBg = (weather) => {
     if (!weather) return;
 
-    if (weather.currentConditions.datetime) {
-        const sunrise = weather.currentConditions.sunrise;
-        const sunset = weather.currentConditions.sunset;
-        const time = weather.currentConditions.datetime;
+    const { datetime, sunrise, sunset } = weather.currentConditions;
 
-        const isNight = sunrise > time || sunset < time;
+    const hour = parseInt(datetime.split(":").shift());
+    let bgClass = "morning-bg";
 
-        document.body.classList.remove("day-bg", "night-bg");
-        document.body.classList.add(isNight ? "night-bg" : "day-bg");
+    if (datetime >= sunrise && hour < 12) {
+        bgClass = "morning-bg";
+    } else if (hour >= 12 && datetime < sunset) {
+        bgClass = "day-bg";
+    } else if (datetime >= sunset && hour < 21) {
+        bgClass = "evening-bg";
+    } else {
+        bgClass = "night-bg";
     }
+
+    document.body.classList.remove(
+        "morning-bg",
+        "day-bg",
+        "evening-bg",
+        "night-bg"
+    );
+    document.body.classList.add(bgClass);
 };
 
 export default changeBg;
